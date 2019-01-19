1c9a746a2b51f46c1478c3b7e9bbf413dc431aba
羅泉恆 <quenlo510@gmail.com>
2019-01-19 16:28:11 +0800

    add level setting sample

--- testLogger.py
+++ testLogger.py
@@ -1,8 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import logging
 import logging.config
 import logging_config
 from logging_config import RotatingFileNameHandler
 
+# set logging level: DEBUG, INFO, WARNING, ERROR, CRITICAL
+LEVEL = "ERROR"
+LOGGING = logging_config.LOGGING
+LOGGING["root"]["level"] = LOGGING["handlers"]["default"]["level"] = LEVEL
 logging.config.dictConfig(logging_config.LOGGING)
 
 # create logger
@@ -15,3 +22,8 @@ logger.info("info message")
 logger.warn("warn message")
 logger.error("error message")
 logger.critical("critical message")
+
+try:
+    print( "AAAAA" + 1+90 )
+except Exception, e:
+    logger.error(e)
